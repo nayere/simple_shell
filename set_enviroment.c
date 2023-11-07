f8221cc9c4c82280deccf0a121431b4df6afd85f
Job  Nayere <37332656+nayere@users.noreply.github.com>
2023-11-07 22:28:15 +0300

    Create set_enviroment.c

---
+++ set_enviroment.c
@@ -0,0 +1,33 @@
+#include "shell.h"
+
+/**
+* set_env - set environment variable
+* @var: name of variable
+* @val: value assigned to variable
+*
+* Return: int
+*/
+int set_env(char *var, char *val)
+{
+if (setenv(var, val, 1) != 0)
+{
+fprintf(stderr, "Failure to set env variable %s\n", var);
+return (1);
+}
+return (0);
+}
+/**
+* unset_env - deletes env variable
+* @var: variable to be deleted
+*
+* Return: int
+*/
+int unset_env(char *var)
+{
+if (unsetenv(var) != 0)
+{
+fprintf(stderr, "Failed to unset variable %s\n", var);
+return (1);
+}
+return (0);
+}
