b96f2e6a2be9d4793383066dc6e685061f5b9a9b
Job  Nayere <37332656+nayere@users.noreply.github.com>
2023-11-07 22:36:34 +0300

    Memory management

---
+++ memory.c
@@ -0,0 +1,108 @@
+#include "shell.h"
+
+/**
+ **_realloc -  Reallocates A Memory Block Using Malloc And Free
+ *@ptr: Pointer
+ *@old_size: Previous Size Of The Pointer
+ *@new_size: New Size Of The Pointer
+ *Return: Void Pointer Rellocated Memory
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *res;
+
+	if (new_size == old_size)
+		return (ptr);
+	if (new_size == 0 && ptr)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	res = malloc(new_size);
+	if (res == NULL)
+		return (NULL);
+	if (ptr == NULL)
+	{
+		fill_an_array(res, '\0', new_size);
+		free(ptr);
+	}
+	else
+	{
+		_memcpy(res, ptr, old_size);
+		free(ptr);
+	}
+	return (res);
+
+}
+/**
+ * free_all - Free Array Of Char Pointer And Char Pointer
+ * @input:Array Pointer
+ * @line:Char Pointer
+ * Return: Void
+ */
+void free_all(char **input, char *line)
+{
+	free(input);
+	free(line);
+	line = NULL;
+	input = NULL;
+}
+
+/**
+ * _memcpy - Copy Byte From Source To Destination
+ * @dest: Destination Pointer
+ * @src: Source Pointer
+ * @n: Size (How Much You Will Copy)
+ *Return: Void Pointer
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
+/**
+ * fill_an_array - Fill An Array By Constant Byte
+ * @a:Void Pointer
+ * @n: Int
+ * @len:Length Int
+ *Return: Void Pointer
+ */
+void *fill_an_array(void *a, int n, unsigned int len)
+{
+	char *p = a;
+	unsigned int i = 0;
+
+	while (i < len)
+	{
+		*p = n;
+		p++;
+		i++;
+	}
+	return (a);
+}
+/**
+ * _calloc -  Allocates Memory For An Array, Using Malloc.
+ * @size: Size
+ * Return: Void Pointer
+ */
+void *_calloc(unsigned int size)
+{
+	char *c;
+	unsigned int i;
+
+	if (size == 0)
+	return (NULL);
+	c = malloc(size);
+	if (c == NULL)
+	return (NULL);
+	for (i = 0; i < size; i++)
+	{
+		c[i] = '\0';
+	}
+	return (c);
+}
---
+++ more_bul.c
@@ -0,0 +1,68 @@
+#include "shell.h"
+
+/**
+ * history_dis - Display History Of User Input Simple Shell
+ * @c:Parsed Command
+ * @s:Statue Of Last Excute
+ * Return: 0 Succes -1 Fail
+ */
+int history_dis(__attribute__((unused))char **c, __attribute__((unused))int s)
+{
+	char *filename = ".shell_history";
+	FILE *ff;
+	char *line = NULL;
+	size_t len = 0;
+	int n = 0;
+	char *ch;
+
+	ff = fopen(filename, "r");
+	if (ff == NULL)
+	{
+		return (-1);
+	}
+	while ((getline(&line, &len, ff)) != -1)
+	{
+		n++;
+		ch = _itoa(n);
+		PRINTER(ch);
+		free(ch);
+		PRINTER(" ");
+		PRINTER(line);
+
+	}
+	if (line)
+		free(line);
+	fclose(ff);
+	return (0);
+}
+/**
+ * print_echo - Excute Normal Echo
+ * @input: Parsed Command
+ * Return: int
+ */
+int print_echo(char **input)
+{
+	int status;
+	pid_t mypid;
+
+	mypid = fork();
+	if (mypid == 0)
+	{
+	if (execve("/bin/echo", input, environ) == -1)
+	{
+		return (-1);
+	}
+		exit(EXIT_FAILURE);
+	}
+	else if (mypid < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		do {
+			waitpid(mypid, &status, WUNTRACED);
+		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
+	}
+	return (1);
+}
