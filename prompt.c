fff2661b37a6611b43d96189e16e710ffb1fe28c
Job  Nayere <37332656+nayere@users.noreply.github.com>
2023-11-10 10:33:34 +0300

    Its Friday and I need just to chill out afterwards

---
+++ prompt.c
@@ -0,0 +1,29 @@
+#include "shell.h"
+
+/**
+ * prompt - Display Shell Prompt
+ */
+void prompt(void)
+{
+	PRINTER("$ ");
+}
+/**
+ * print_error - Display Error Based on Command and How Many Time Shell Looped
+ * @input:User Input
+ * @count:Simple Shell Count Loop
+ * @argv:Program Name
+ * Return: Void
+ */
+void print_error(char *input, int count, char **argv)
+{
+	char *ch;
+
+	PRINTER(argv[0]);
+	PRINTER(": ");
+	ch = _itoa(count);
+	PRINTER(ch);
+	free(ch);
+	PRINTER(": ");
+	PRINTER(input);
+	PRINTER(": input not found\n");
+}
