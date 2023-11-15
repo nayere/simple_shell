79a8c99d8448cb71822da257884cc1c8bd42d7d7
Job  Nayere <37332656+nayere@users.noreply.github.com>
2023-11-15 11:16:46 +0300

    Update shell.c

--- shell.c
+++ shell.c
@@ -1,7 +1,5 @@
 #include "shell.h"
 
-/** Authors Mwangii & Nayere */
-
 /**
 * main - Simple Shell (Hsh)
 * @argc: Argument Count
@@ -19,58 +17,62 @@ if (argv[1] != NULL)
 read_file(argv[1], argv);
 signal(SIGINT, signal_to_handel);
 while (statue)
-	{
-		counter++;
-		if (isatty(STDIN_FILENO))
-			prompt();
-		input = _getline();
-		if (input[0] == '\0')
-		{
-			continue;
-		}
-			history(input);
-			cmd = parse_cmd(input);
-			if (_strcmp(cmd[0], "exit") == 0)
-			{
-				exit_bul(cmd, input, argv, counter);
-			}
-			else if (_strcmp(cmd[0], "setenv") == 0)
-			{
-				if (cmd[1] != NULL && cmd[2] != NULL)
-				{
-					res = set_env(cmd[1], cmd[2]);
-					if (res != 0)
-					{
-						fprintf(stderr, "set_env failure\n");
-					}
-					else
-						fprintf(stderr, "incorrect set_env usage\n");
-				}
-			}
-			else if (_strcmp(cmd[0], "unsetenv") == 0)
-			{
-				if (cmd[1] != NULL)
-				{
-				res = unset_env(cmd[1]);
-					if (res != 0)
-						fprintf(stderr, "failed to unset_env\n");
-			else
-				fprintf(stderr, "incorrect unset_env usage");
-		}
+{
+counter++;
+if (isatty(STDIN_FILENO))
+prompt();
+input = _getline();
+if (input[0] == '\0')
+{
+continue;
+}
+history(input);
+cmd = parse_cmd(input);
+
+if (_strcmp(cmd[0], "exit") == 0)
+{
+exit_bul(cmd, input, argv, counter);
+}
+else if (_strcmp(cmd[0], "setenv") == 0)
+{
+
+if (cmd[1] != NULL && cmd[2] != NULL)
+{
+res = set_env(cmd[1], cmd[2]);
+
+if (res != 0)
+{
+fprintf(stderr, "set_env failure\n");
+}
+else
+fprintf(stderr, "incorrect set_env usage\n");
+}
+}
+else if (_strcmp(cmd[0], "unsetenv") == 0)
+{
+if (cmd[1] != NULL)
+{
+res = unset_env(cmd[1]);
+					
+if (res != 0)
+fprintf(stderr, "failed to unset_env\n");
+else
+fprintf(stderr, "incorrect unset_env usage");
+}
 }
 else if (check_builtin(cmd) == 0)
 {
-	st = handle_builtin(cmd, st);
-	free_all(cmd, input);
-	continue;
+st = handle_builtin(cmd, st);
+free_all(cmd, input);
+continue;
 }
 else
 {
-	st = check_cmd(cmd, input, counter, argv);
+st = check_cmd(cmd, input, counter, argv);
 }
-		free_all(cmd, input);
-	}
-	return (statue);
+free_all(cmd, input);
+}
+return (statue);
 }
 
 /**
