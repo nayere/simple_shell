1fe37ca31579d0296e13b1dd080eabd29f6a1828
Job  Nayere <37332656+nayere@users.noreply.github.com>
2023-11-15 12:26:18 +0300

    Update shell.c

--- shell.c
+++ shell.c
@@ -3,100 +3,107 @@
 /** Authors Mwangii & Nayere */
 
 /**
- * main - Simple Shell (Hsh)
- * @argc: Argument Count
- * @argv: Argument Value
- * Return: Exit Value By Status
- */
+* main - Simple Shell (Hsh)
+* @argc: Argument Count
+* @argv:Argument Value
+* Return: Exit Value By Status
+*/
+
 int main(__attribute__((unused)) int argc, char **argv)
+
 {
 int counter = 0, statue = 1, st = 0, res;
 char *input, **cmd;
 
-setup_initial(argc, argv);
 
-while (statue)
-{
-    counter++;
-    input = get_input();
+	if (argv[1] != NULL)
+		read_file(argv[1], argv);
+	signal(SIGINT, signal_to_handel);
+	while (statue)
+	{
+		counter++;
+		if (isatty(STDIN_FILENO))
+			prompt();
+		input = _getline();
+		if (input[0] == '\0')
+		{
+			continue;
+		}
+			history(input);
+			cmd = parse_cmd(input);
+			if (_strcmp(cmd[0], "exit") == 0)
+			{
+				exit_bul(cmd, input, argv, counter);
+			}
+			else if (_strcmp(cmd[0], "setenv") == 0)
+			{
+				if (cmd[1] != NULL && cmd[2] != NULL)
+				{
+					res = set_env(cmd[1], cmd[2]);
+					if (res != 0)
+					{
+						fprintf(stderr, "set_env failure\n");
+					}
+					else
+						fprintf(stderr, "incorrect set_env usage\n");
+				}
+			}
+			else if (_strcmp(cmd[0], "unsetenv") == 0)
+			{
+				if (cmd[1] != NULL)
+				{
+					res = unset_env(cmd[1]);
+					if (res != 0)
+						fprintf(stderr, "failed to unset_env\n");
+					else
+						fprintf(stderr, "incorrect unset_env usage");
+				}
+			}
+			else if (check_builtin(cmd) == 0)
+			{
+				st = handle_builtin(cmd, st);
+				free_all(cmd, input);
+				continue;
+			}
+			else
+			{
+				st = check_cmd(cmd, input, counter, argv);
 
-    if (input[0] == '\0')
-        continue;
-
-        handle_input(input, &cmd, &counter, argv, &st, &res);
-        free_input_and_cmd(&cmd, input);
-    }
-
-    return statue;
+			}
+			free_all(cmd, input);
+		}
+	return (statue);
 }
 
 /**
- * setup_initial - Perform initial setup
- * @argc: Argument Count
- * @argv: Argument Value
- */
-void setup_initial(int argc, char **argv)
+* check_builtin - check builtin
+*
+* @cmd:command to check
+* Return: 0 Succes -1 Fail
+*/
+int check_builtin(char **cmd)
 {
-    if (argv[1] != NULL)
-        read_file(argv[1], argv);
-    signal(SIGINT, signal_to_handel);
-    (void)argc;
+bul_t fun[] = {
+
+{"echo", NULL},
+{"history", NULL},
+{"cd", NULL},
+{"help", NULL},
+{NULL, NULL}
+};
+int i = 0;
+if (*cmd == NULL)
+{
+return (-1);
 }
 
-/**
- * get_input - Get user input
- * Return: User input
- */
-char *get_input()
+while ((fun + i)->command)
 {
-    if (isatty(STDIN_FILENO))
-        prompt();
-    return _getline();
+if (_strcmp(cmd[0], (fun + i)->command) == 0)
+return (0);
+i++;
+}
+return (-1);
 }
 
-/**
- * handle_input - Process user input
- * @input: User input
- * @cmd: Command value
- * @counter: Counter value
- * @argv: Argument value
- * @st: Status value
- * @res: Result value
- */
-void handle_input(char *input, char ***cmd, int *counter, char **argv, int *st, int *res)
-{
-    history(input);
-    *cmd = parse_cmd(input);
-
-    if (_strcmp((*cmd)[0], "exit") == 0)
-    {
-        exit_bul(*cmd, input, argv, *counter);
-    }
-    else if (_strcmp((*cmd)[0], "setenv") == 0)
-    {
-        handle_setenv(*cmd, res);
-    } 
-    else if (_strcmp((*cmd)[0], "unsetenv") == 0)
-    
-        handle_unsetenv(*cmd, res);
-    }
-    else if (check_builtin(*cmd) == 0)
-    {
-        *st = handle_builtin_command(*cmd, *st);
-        free_all(*cmd, input);
-    }
-    else
-    {
-        *st = check_and_handle_cmd(*cmd, input, *counter, argv);
-    }
-}
-
-/**
- * free_input_and_cmd - Free input and command
- * @cmd: Command value
- * @input: User input
- */
-void free_input_and_cmd(char ***cmd, char *input)
-{
-    free_all(*cmd, input);
-}
+/** Authors Mwangii & Nayere */
