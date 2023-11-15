7f01ab88d910fb74457bf052122682149fdf9d0b
Job  Nayere <37332656+nayere@users.noreply.github.com>
2023-11-15 11:42:52 +0300

    Update shell.c

--- shell.c
+++ shell.c
@@ -7,74 +7,64 @@
  * Return: Exit Value By Status
  */
 
-int main(__attribute__((unused)) int argc, char **argv)
-{
-	char *input, **cmd;
-	int counter = 0, status = 1, st = 0;
+int main(__attribute__((unused)) int argc, char **argv) {
+    int counter = 0;
+    char *input, **cmd;
 
-	if (argv[1] != NULL)
-		read_file(argv[1], argv);
-	signal(SIGINT, signal_to_handel);
-	while (status)
-	{
-		counter++;
-		if (isatty(STDIN_FILENO))
-			prompt();
-		input = _getline();
-		if (input[0] == '\0')
-		{
-			continue;
-		}
-		history(input);
-		cmd = parse_cmd(input);
-		if (_strcmp(cmd[0], "exit") == 0)
-		{
-			exit_builtin(cmd, input, argv, counter);
-		}
-		else if (check_builtin(cmd) == 0)
-		{
-			st = handle_builtin(cmd, st);
-			free_all(cmd, input);
-			continue;
-		}
-		else
-		{
-			st = check_cmd(cmd, input, counter, argv);
+    setup_initial(argv);
 
-		}
-		free_all(cmd, input);
-	}
-	return (status);
+    while (evaluate_statue()) {
+        counter++;
+        input = get_input();
+
+        if (input[0] == '\0') {
+            continue;
+        }
+
+        process_input(input, &cmd, &counter, argv);
+        free_input_and_cmd(&cmd, input);
+    }
+
+    return get_statue();
 }
-/**
- * check_builtin - check builtin
- *
- * @cmd:command to check
- * Return: 0 Succes -1 Fail
- */
-int check_builtin(char **cmd)
-{
-	bul_t fun[] = {
-		{"cd", NULL},
-		{"help", NULL},
-		{"echo", NULL},
-		{"history", NULL},
-		{NULL, NULL}
-	};
-	int i = 0;
-		if (*cmd == NULL)
-	{
-		return (-1);
-	}
 
-	while ((fun + i)->command)
-	{
-		if (_strcmp(cmd[0], (fun + i)->command) == 0)
-			return (0);
-		i++;
-	}
-	return (-1);
+void setup_initial(char **argv) {
+    if (argv[1] != NULL)
+        read_file(argv[1], argv);
+    signal(SIGINT, signal_to_handel);
 }
+
+int evaluate_statue() {
+    return statue;
+}
+
+char *get_input() {
+    if (isatty(STDIN_FILENO))
+        prompt();
+    return _getline();
+}
+
+void process_input(char *input, char ***cmd, int *counter, char **argv) {
+    history(input);
+    *cmd = parse_cmd(input);
+
+    if (_strcmp((*cmd)[0], "exit") == 0) {
+        exit_bul(*cmd, input, argv, *counter);
+    } else if (_strcmp((*cmd)[0], "setenv") == 0) {
+        handle_setenv(*cmd);
+    } else if (_strcmp((*cmd)[0], "unsetenv") == 0) {
+        handle_unsetenv(*cmd);
+    } else if (check_builtin(*cmd) == 0) {
+        handle_builtin_command(*cmd);
+    } else {
+        handle_other_commands(*cmd, input, *counter, argv);
+    }
+}
+
+void free_input_and_cmd(char ***cmd, char *input) {
+    free_all(*cmd, input);
+}
+
 /**
  * creat_envi - Creat Array of Enviroment Variable
  * @envi: Array of Enviroment Variable
