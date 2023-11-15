f2d5ef599dd53b4b277df3dd2eb311213f76b489
Job  Nayere <37332656+nayere@users.noreply.github.com>
2023-11-15 10:42:53 +0300

    Update shell.c

--- shell.c
+++ shell.c
@@ -3,13 +3,14 @@
 /** By Nayere */
 
 /**
- * main - Simple Shell (Hsh)
- * @argc: Argument Count
- * @argv: Argument Value
- * Return: Exit Value By Status
- */
+* main - Simple Shell (Hsh)
+* @argc: Argument Count
+* @argv: Argument Value
+* Return: Exit Value By Status
+*/
 
-int main(__attribute__((unused)) int argc, char **argv) {
+int main(__attribute__((unused)) int argc, char **argv) 
+{
     int counter = 0, statue = 1, st = 0, res;
     char *input, **cmd;
 
@@ -17,43 +18,59 @@ int main(__attribute__((unused)) int argc, char **argv) {
         read_file(argv[1], argv);
     signal(SIGINT, signal_to_handel);
     
-    while (statue) {
+    while (statue) 
+    {
         counter++;
         if (isatty(STDIN_FILENO))
             prompt();
         input = _getline();
         
-        if (input[0] == '\0') {
+        if (input[0] == '\0') 
+        {
             continue;
         }
         
         history(input);
         cmd = parse_cmd(input);
         
-        if (_strcmp(cmd[0], "exit") == 0) {
+        if (_strcmp(cmd[0], "exit") == 0)
+        {
             exit_bul(cmd, input, argv, counter);
-        } else if (_strcmp(cmd[0], "setenv") == 0) {
-            if (cmd[1] != NULL && cmd[2] != NULL) {
+        } 
+        else if (_strcmp(cmd[0], "setenv") == 0)
+        {
+            if (cmd[1] != NULL && cmd[2] != NULL)
+            {
                 res = set_env(cmd[1], cmd[2]);
-                if (res != 0) {
+                if (res != 0)
+                {
                     fprintf(stderr, "set_env failure\n");
-                } else {
+                }
+                else 
+                {
                     fprintf(stderr, "incorrect set_env usage\n");
                 }
             }
-        } else if (_strcmp(cmd[0], "unsetenv") == 0) {
-            if (cmd[1] != NULL) {
+        } 
+        else if (_strcmp(cmd[0], "unsetenv") == 0)
+        {
+            if (cmd[1] != NULL)
+            {
                 res = unset_env(cmd[1]);
                 if (res != 0)
                     fprintf(stderr, "failed to unset_env\n");
                 else
                     fprintf(stderr, "incorrect unset_env usage");
             }
-        } else if (check_builtin(cmd) == 0) {
+        }
+        else if (check_builtin(cmd) == 0)
+        {
             st = handle_builtin(cmd, st);
             free_all(cmd, input);
             continue;
-        } else {
+        }
+        else
+        {
             st = check_cmd(cmd, input, counter, argv);
         }
         
@@ -63,11 +80,12 @@ int main(__attribute__((unused)) int argc, char **argv) {
 }
 
 /**
- * check_builtin - check builtin
- * @cmd: command to check
- * Return: 0 Success -1 Fail
- */
-int check_builtin(char **cmd) {
+* check_builtin - check builtin
+* @cmd: command to check
+* Return: 0 Success -1 Fail
+*/
+int check_builtin(char **cmd) 
+{
     bul_t fun[] = {
         {"echo", NULL},
         {"history", NULL},
@@ -77,7 +95,8 @@ int check_builtin(char **cmd) {
     };
     int i = 0;
     
-    if (*cmd == NULL) {
+    if (*cmd == NULL)
+    {
         return -1;
     }
 
@@ -94,7 +113,8 @@ int check_builtin(char **cmd) {
  * @envi: Array of Environment Variable
  * Return: Void
  */
-void creat_envi(char **envi) {
+void creat_envi(char **envi)
+{
     int i;
     
     for (i = 0; environ[i]; i++)
