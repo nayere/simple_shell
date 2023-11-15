0254d579e4b057a62329c41c78a94dfbceafb1ee
Job  Nayere <37332656+nayere@users.noreply.github.com>
2023-11-15 11:51:28 +0300

    Update shell.c

--- shell.c
+++ shell.c
@@ -6,7 +6,6 @@
  * @argv:Argument Value
  * Return: Exit Value By Status
  */
-
 int main(__attribute__((unused)) int argc, char **argv) {
     int counter = 0;
     char *input, **cmd;
@@ -28,41 +27,74 @@ int main(__attribute__((unused)) int argc, char **argv) {
     return get_statue();
 }
 
+/**
+ * setup_initial - Perform initial setup
+ * @argv: Argument value
+ */
 void setup_initial(char **argv) {
     if (argv[1] != NULL)
         read_file(argv[1], argv);
     signal(SIGINT, signal_to_handel);
 }
 
+/**
+ * evaluate_statue - Evaluate the statue condition
+ * Return: Statue value
+ */
 int evaluate_statue() {
     return statue;
 }
 
+/**
+ * get_input - Get user input
+ * Return: User input
+ */
 char *get_input() {
     if (isatty(STDIN_FILENO))
         prompt();
     return _getline();
 }
 
+/**
+ * process_input - Process user input
+ * @input: User input
+ * @cmd: Command value
+ * @counter: Counter value
+ * @argv: Argument value
+ */
 void process_input(char *input, char ***cmd, int *counter, char **argv) {
     history(input);
     *cmd = parse_cmd(input);
-
-    if (_strcmp((*cmd)[0], "exit") == 0) {
-        exit_bul(*cmd, input, argv, *counter);
-    } else if (_strcmp((*cmd)[0], "setenv") == 0) {
-        handle_setenv(*cmd);
-    } else if (_strcmp((*cmd)[0], "unsetenv") == 0) {
-        handle_unsetenv(*cmd);
-    } else if (check_builtin(*cmd) == 0) {
-        handle_builtin_command(*cmd);
-    } else {
-        handle_other_commands(*cmd, input, *counter, argv);
+    if (_strcmp((*cmd)[0], "exit") == 0)
+    {
+	    exit_bul(*cmd, input, argv, *counter);
+    }
+    else if (_strcmp((*cmd)[0], "setenv") == 0)
+    {
+	    handle_setenv(*cmd);
+    }
+    else if (_strcmp((*cmd)[0], "unsetenv") == 0)
+    {
+	    handle_unsetenv(*cmd);
+    }
+    else if (check_builtin(*cmd) == 0)
+    {
+	    handle_builtin_command(*cmd);
+    } 
+    else
+    {
+	    handle_other_commands(*cmd, input, *counter, argv);
     }
 }
 
-void free_input_and_cmd(char ***cmd, char *input) {
-    free_all(*cmd, input);
+/**
+ * free_input_and_cmd - Free input and command
+ * @cmd: Command value
+ * @input: User input
+ */
+void free_input_and_cmd(char ***cmd, char *input) 
+{
+	free_all(*cmd, input);
 }
 
 /**
