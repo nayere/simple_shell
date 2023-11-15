dab4bc9f3d320722202657130872f24c2f623080
Job  Nayere <37332656+nayere@users.noreply.github.com>
2023-11-15 12:31:13 +0300

    Update shell.c

--- shell.c
+++ shell.c
@@ -1,20 +1,16 @@
 #include "shell.h"
 
-/** Authors Mwangii & Nayere */
-
 /**
-* main - Simple Shell (Hsh)
-* @argc: Argument Count
-* @argv:Argument Value
-* Return: Exit Value By Status
-*/
+ * main - Simple Shell (Hsh)
+ * @argc: Argument Count
+ * @argv:Argument Value
+ * Return: Exit Value By Status
+ */
 
 int main(__attribute__((unused)) int argc, char **argv)
-
 {
-int counter = 0, statue = 1, st = 0, res;
-char *input, **cmd;
-
+	char *input, **cmd;
+	int counter = 0, statue = 1, st = 0;
 
 	if (argv[1] != NULL)
 		read_file(argv[1], argv);
@@ -29,95 +25,66 @@ char *input, **cmd;
 		{
 			continue;
 		}
-			history(input);
-			cmd = parse_cmd(input);
-			if (_strcmp(cmd[0], "exit") == 0)
-			{
-				exit_bul(cmd, input, argv, counter);
-			}
-			else if (_strcmp(cmd[0], "setenv") == 0)
-			{
-				if (cmd[1] != NULL && cmd[2] != NULL)
-				{
-					res = set_env(cmd[1], cmd[2]);
-					if (res != 0)
-					{
-						fprintf(stderr, "set_env failure\n");
-					}
-					else
-						fprintf(stderr, "incorrect set_env usage\n");
-				}
-			}
-			else if (_strcmp(cmd[0], "unsetenv") == 0)
-			{
-				if (cmd[1] != NULL)
-				{
-					res = unset_env(cmd[1]);
-					if (res != 0)
-						fprintf(stderr, "failed to unset_env\n");
-					else
-						fprintf(stderr, "incorrect unset_env usage");
-				}
-			}
-			else if (check_builtin(cmd) == 0)
-			{
-				st = handle_builtin(cmd, st);
-				free_all(cmd, input);
-				continue;
-			}
-			else
-			{
-				st = check_cmd(cmd, input, counter, argv);
-
-			}
-			free_all(cmd, input);
+		history(input);
+		cmd = parse_cmd(input);
+		if (_strcmp(cmd[0], "exit") == 0)
+		{
+			exit_bul(cmd, input, argv, counter);
 		}
+		else if (check_builtin(cmd) == 0)
+		{
+			st = handle_builtin(cmd, st);
+			free_all(cmd, input);
+			continue;
+		}
+		else
+		{
+			st = check_cmd(cmd, input, counter, argv);
+
+		}
+		free_all(cmd, input);
+	}
 	return (statue);
 }
-
 /**
-* check_builtin - check builtin
-*
-* @cmd:command to check
-* Return: 0 Succes -1 Fail
-*/
+ * check_builtin - check builtin
+ *
+ * @cmd:command to check
+ * Return: 0 Succes -1 Fail
+ */
 int check_builtin(char **cmd)
 {
-bul_t fun[] = {
+	bul_t fun[] = {
+		{"cd", NULL},
+		{"help", NULL},
+		{"echo", NULL},
+		{"history", NULL},
+		{NULL, NULL}
+	};
+	int i = 0;
+		if (*cmd == NULL)
+	{
+		return (-1);
+	}
 
-{"echo", NULL},
-{"history", NULL},
-{"cd", NULL},
-{"help", NULL},
-{NULL, NULL}
-};
-int i = 0;
-if (*cmd == NULL)
-{
-return (-1);
+	while ((fun + i)->command)
+	{
+		if (_strcmp(cmd[0], (fun + i)->command) == 0)
+			return (0);
+		i++;
+	}
+	return (-1);
 }
-
-while ((fun + i)->command)
-{
-if (_strcmp(cmd[0], (fun + i)->command) == 0)
-return (0);
-i++;
-}
-return (-1);
-}
-
 /**
-* creat_envi - Creat Array of Enviroment Variable
-* @envi: Array of Enviroment Variable
-* Return: Void
-*/
+ * creat_envi - Creat Array of Enviroment Variable
+ * @envi: Array of Enviroment Variable
+ * Return: Void
+ */
 void creat_envi(char **envi)
 {
-int i;
+	int i;
 
-for (i = 0; environ[i]; i++)
-envi[i] = _strdup(environ[i]);
-envi[i] = NULL;
+	for (i = 0; environ[i]; i++)
+		envi[i] = _strdup(environ[i]);
+	envi[i] = NULL;
 }
-
-/** Authors Mwangii & Nayere */
